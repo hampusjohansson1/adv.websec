9abb19b160aa7090343ecdab82fa17932e45ba50
Hampus Johansson <hampus@Hampuss-MacBook-Pro.local>
2017-12-08 15:48:05 +0100

    OAEP works

--- Assignment4 - Web/OAEP.py
+++ Assignment4 - Web/OAEP.py
@@ -35,7 +35,7 @@ def MGF1 (mgfSeed, maskLen):
 
     return T[:maskLen*2];
 
-def OAEPencode(message):
+def OAEPencode(message,seed):
     L = "";
     k = 128;
     mLen = len(bytearray.fromhex(message));
@@ -48,7 +48,6 @@ def OAEPencode(message):
 
     DB = lHash + PS +  bytearray.fromhex("01") + bytearray.fromhex(message);
 
-    seed = "1e652ec152d0bfcd65190ffc604c0933d0423381";
     dbMask = MGF1(seed,k-hLen-1);
 
     maskedDB = hex(int(DB.hex(), 16) ^ int(dbMask, 16));
@@ -58,12 +57,7 @@ def OAEPencode(message):
     maskedSeed = hex(int(seed, 16) ^ int(seedMask, 16));
     maskedSeed = str(maskedSeed)[2:];
 
-    print("maskedSeed: " + maskedSeed);
-    print("MaskedDB: " + maskedDB);
-
     EM = "0000" + maskedSeed + maskedDB;
-
-   # print(EM);
 
     return EM;
 
@@ -71,19 +65,36 @@ def OAEPdecode(EM):
     L = "";
     k = 128;
     hLen = 20;
-    lHash = hashlib.sha1(L.encode("utf-8")).digest();
+    M = "";
 
     EM = EM[4:];
     maskedSeed = EM[:hLen*2-2];
     maskedDB = EM[hLen*2-2:];
 
-    return EM;
+    seedMask = MGF1(maskedDB, hLen);
 
-#mgfSeed = "0123456789abcdef";
-#maskLen = 30;
-#output = MGF1(mgfSeed,30);
-#print(output);
+    seed = hex(int(maskedSeed, 16) ^ int(seedMask, 16))[2:];
+
+    dbMask = MGF1(seed, k - hLen - 1);
+
+    DB = hex(int(maskedDB, 16) ^ int(dbMask, 16))[2:];
+
+    #remove lHash
+    DB = DB[hLen*2:];
+
+    for i,c in enumerate(DB):
+        if c == "1":
+            M = DB[i+1:];
+            break;
+
+    return M;
 
 message = "fd5507e917ecbe833878";
-EM = OAEPencode(message);
-OAEPdecode(EM);
+seed = "1e652ec152d0bfcd65190ffc604c0933d0423381";
+print (message);
+
+EM = OAEPencode(message,seed);
+print(EM);
+
+mesret = OAEPdecode(EM);
+print(mesret);
